9cd3d49fa44eda0dccb3f6d786ec49cb90d2f28b
Martin Triay <martriay@gmail.com>
2020-09-11 16:25:01 -0300

    Bump ganache and upgrade script

--- scripts/upgradeBoxToV3.js
+++ scripts/upgradeBoxToV3.js
@@ -2,9 +2,10 @@ const { ethers, upgrades } = require("@nomiclabs/buidler");
 const OZ_SDK_EXPORT = require("../openzeppelin-cli-export.json");
 
 async function main() {
-  const [ BoxV2 ] = OZ_SDK_EXPORT.rinkeby.Box;
+  const [ BoxV2 ] = OZ_SDK_EXPORT.networkFiles.rinkeby.proxies["openzeppelin-upgrades-migration-example/Box"];
   const BoxV3 = await ethers.getContractFactory("BoxV3");
-  await upgrades.upgradeProxy(BoxV2, BoxV3);
+  const foo = await upgrades.upgradeProxy(BoxV2.address, BoxV3);
+  console.log(foo);
 }
 
 main();
