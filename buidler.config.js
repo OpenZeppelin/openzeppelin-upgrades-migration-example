02f76830e9ab08ea985ecdad2c2aad3608aa2f59
Martin Triay <martriay@gmail.com>
2020-09-22 19:25:19 -0300

    Add migrated compiler settings

--- buidler.config.js
+++ buidler.config.js
@@ -18,6 +18,10 @@ task("accounts", "Prints the list of accounts", async () => {
 module.exports = {
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
-    version: "0.6.8",
+    version: "0.6.12",
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    }
   },
 };
